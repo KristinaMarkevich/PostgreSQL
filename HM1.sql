27e899637a1f90e90c33f2baf83481a88ec18bf4
Kristina <christin.paint@gmail.com>
2023-06-27 13:03:27 +0300

    Update HM1.sql

--- HM1.sql
+++ HM1.sql
@@ -1,29 +1,20 @@
+1. Создать таблицу employees
 create table employees(
 id serial primary key,
 employee_name varchar(50) not null
 );
-
+2. Наполнить таблицу employee 70 строками. Наполняем разными данными, вызываем несколько раз функцию.
 insert into employees(employee_name)
 values ('Kris15'),
        ('Nikita15'),
        ('Nilou15'),
        ('Zamay15'),
        ('Gleb15');
-
-
-select * from employees;
-
-delete from employees
-where id > 70 ;
-
+3.Создать таблицу salary   
 create table salary (
 id serial primary key,
 monthly_salary int not null);
- 
-select* from salary
-
-drop table employee_salary cascade
-
+4.Наполнить таблицу salary 15 строками:
 insert into salary (monthly_salary)
 values (1000),
        (1100),
@@ -41,14 +32,15 @@ values (1000),
        (2300),
        (2400),
        (2500);
-       
+5. Создать таблицу employee_salary
 create table employee_salary(
 id serial primary key,
 employee_id int not null unique,
 salary_id int not null,
 foreign key (salary_id) references salary(id)
 );
-
+6.Наполнить таблицу employee_salary 40 строками:
+- в 10 строк из 40 вставить несуществующие employee_id
 insert into employee_salary(employee_id, salary_id)
 values (53,1),
        (32,10),
@@ -90,21 +82,15 @@ values (53,1),
        (26,13),
        (43,5),
        (25,7);
-       
-
-delete from employee_salary
-where id = 
-      
-select * from employee_salary;
-
+7.Создать таблицу roles
 create table roles(
 id serial primary key,
 role_name int not null unique
 );
-
+8.Поменять тип столба role_name с int на varchar(30)
 alter table roles 
 alter column role_name TYPE varchar(30);
-
+9.Наполнить таблицу roles 20 строками
 insert into roles (role_name)
 values ('Junior Python developer'),
        ('Middle Python developer'),
@@ -126,9 +112,7 @@ values ('Junior Python developer'),
        ('Junior Automation QA engineer'),
        ('Middle Automation QA engineer'),
        ('Senior Automation QA engineer');
-       
- select * from roles;
- 
+10. Создать таблицу roles_employee
 create table roles_employee(
 id serial primary key,
 employee_id int not null unique,
@@ -136,7 +120,7 @@ role_id int not null,
 foreign key (employee_id) references employees(id),
 foreign key (role_id) references roles(id)
 );
-
+11. Наполнить таблицу roles_employee 40 строками:
 insert into roles_employee(employee_id, role_id)
 values (56,2),
        (63,15),
@@ -166,18 +150,18 @@ values (56,2),
         (1,9),
         (2,20),
         (3,19),
-(4,16),
-(5,1),
-(11,3),
-(21,13),
-(13,12),
-(41,10),
-(15,5),
-(34,8),
-(38,19),
-(65,20),
-(45,11),
-(31,15);
+        (4,16),
+         (5,1),
+        (11,3),
+        (21,13),
+        (13,12),
+        (41,10),
+        (15,5),
+        (34,8),
+        (38,19),
+        (65,20),
+        (45,11),
+        (31,15);
        
 select * from roles_employee;
 
